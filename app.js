4a0601a4015944847f3cd8a6455ece55f81f8ce2
pedro.gomes <pedro.gomes@sistemapoliedro.com.br>
2021-12-05 19:48:16 -0300

    feat(api): create endpoint to check API health

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
+const http = require('http')
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const cors = require('kcors')
 const respond = require('koa-respond')
 const compress = require('koa-compress')
 const mount = require('koa-mount')
+const { createTerminus } = require('@godaddy/terminus')
 
 const app = new Koa()
 
@@ -17,4 +19,13 @@ app
     .use(bodyParser())
     .use(mount('/api/v1', routes))
 
-module.exports = app
+const server = http.createServer(app.callback())
+
+createTerminus(server, {
+    healthChecks: {
+        '/healthcheck': () => ({ status: 'ok' }),
+        verbatim: true
+    }
+})
+
+module.exports = server
