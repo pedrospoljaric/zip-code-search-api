92faa97e7bf1c5238ed3a751e496c122a69261ec
pedro.gomes <pedro.gomes@sistemapoliedro.com.br>
2021-12-05 22:12:47 -0300

    feat(addresses): add redis cache to address find query

--- models/addresses/findAddressByZipCode.js
+++ models/addresses/findAddressByZipCode.js
@@ -1,1 +1,13 @@
-module.exports = (db) => (zipCode) => db('addresses').where({ zip_code: zipCode }).first()
+const { isUndefined } = require('lodash/fp')
+const redis = require('../../services/redis')
+
+module.exports = (db) => async (zipCode) => {
+    const addressCacheKey = `addresses:${zipCode}`
+    const addressCache = await redis.get(addressCacheKey)
+    if (isUndefined(addressCache)) {
+        const address = await db('addresses').where({ zip_code: zipCode }).first()
+        await redis.set(addressCacheKey, address || null, 86400000)
+        return address
+    }
+    return addressCache
+}
